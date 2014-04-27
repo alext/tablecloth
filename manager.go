1996dee56813725a5d4cdc384f7c45292a21d863
Alex Tomlins <alex@tomlins.org.uk>
2014-04-27 14:24:52 +0100

    Split envmap into separate file

--- manager.go
+++ manager.go
@@ -7,7 +7,6 @@ import (
 	"os/exec"
 	"os/signal"
 	"strconv"
-	"strings"
 	"sync"
 	"syscall"
 	"time"
@@ -213,23 +212,4 @@ func (m *manager) stopTemporaryChild() {
 		//TODO: better error handling
 		return
 	}
-}
-
-type envMap map[string]string
-
-func newEnvMap(env []string) (em envMap) {
-	em = make(map[string]string, len(env))
-	for _, item := range env {
-		parts := strings.SplitN(item, "=", 2)
-		em[parts[0]] = parts[1]
-	}
-	return
-}
-
-func (em envMap) ToEnv() (env []string) {
-	env = make([]string, 0, len(em))
-	for k, v := range em {
-		env = append(env, k+"="+v)
-	}
-	return
 }
